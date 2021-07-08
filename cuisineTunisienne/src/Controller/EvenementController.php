0f5b0a079970b3dc5a2d02f7b23bd12c69e52eab
hichem000003 <87043526+hichem000003@users.noreply.github.com>
2021-07-08 11:58:04 +0100

    EvenementController

---
+++ cuisineTunisienne/src/Controller/EvenementController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Evenement;
+use App\Form\EvenementType;
+use App\Repository\EvenementRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/evenement")
+ */
+class EvenementController extends AbstractController
+{
+    /**
+     * @Route("/", name="evenement_index", methods={"GET"})
+     */
+    public function index(EvenementRepository $evenementRepository): Response
+    {
+        $objects=$evenementRepository->findAll();
+        $encoders=$arrayName = array(new jsonEncoder());
+        $serializer= new Serializer([new ObjectNormalizer()],$encodrs);
+        $data=$serializer->($objects,'json');
+        $response = new Response($data,200);
+        return $response;
+    }
+
+    /**
+     * @Route("/new", name="evenement_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $evenement = new Evenement();
+
+       
+    }
+
+    /**
+     * @Route("/{id}", name="evenement_show", methods={"GET"})
+     */
+    public function show(Evenement $evenement): Response
+    {
+        
+    }
+
+    /**
+     * @Route("/{id}/edit", name="evenement_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Evenement $evenement): Response
+    {
+        $form = $this->createForm(EvenementType::class, $evenement);
+        $form->handleRequest($request);
+
+        
+    }
+
+    /**
+     * @Route("/{id}", name="evenement_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Evenement $evenement): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$evenement->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($evenement);
+            $entityManager->flush();
+        }
+
+        return $this;
+    }
+}
